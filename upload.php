e2bd2e64eba3718f28196108526f141b7194e0ba
Simon East <me@simoneast.net>
2012-06-30 11:40:40 +1000

    Attempt to use PHP on Github-pages.  Dunno if it'll work?

---
+++ upload.php
@@ -0,0 +1,17 @@
+{success:true}
+
+<?php
+exit;
+// list of valid extensions, ex. array("jpeg", "xml", "bmp")
+$allowedExtensions = array();
+// max file size in bytes
+$sizeLimit = 10 * 1024 * 1024;
+
+require('UploaderClass.php');
+$uploader = new qqFileUploader($allowedExtensions, $sizeLimit);
+
+// Call handleUpload() with the name of the folder, relative to PHP's getcwd()
+// $result = $uploader->handleUpload('uploads/');
+
+// to pass data through iframe you will need to encode all html tags
+echo htmlspecialchars(json_encode($result), ENT_NOQUOTES);
