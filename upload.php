f7665b1b97b96120199302595f73127b75cb8625
Simon East <me@simoneast.net>
2012-07-01 12:35:06 +1000

    Moved demo POST to more reliable server

--- upload.php
+++ upload.php
@@ -1,7 +1,14 @@
-{success:true}
-
 <?php
+// This file can be placed on any PHP-enabled server (not Github) to demo the uploading
+// File is simply discarded, not saved, and always returns success: true
+
+header('Access-Control-Allow-Headers: origin, x-mime-type, x-requested-with, x-file-name, content-type');
+header('Access-Control-Allow-Origin: *');
+echo '{success:true}';
+
+/***
 exit;
+
 // list of valid extensions, ex. array("jpeg", "xml", "bmp")
 $allowedExtensions = array();
 // max file size in bytes
@@ -15,3 +22,4 @@ $uploader = new qqFileUploader($allowedExtensions, $sizeLimit);
 
 // to pass data through iframe you will need to encode all html tags
 echo htmlspecialchars(json_encode($result), ENT_NOQUOTES);
+***/
